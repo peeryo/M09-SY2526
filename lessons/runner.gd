628c83270b774fb2da2e494785630192752182ae
[peeryo] <peeryo@sfusd.edu>
2025-12-06 08:13:09 -0800

    M9L5 completed

--- lessons/runner.gd
+++ lessons/runner.gd
@@ -1,20 +1,8 @@
 extends CharacterBody2D
-@onready var _skin: Sprite2D = %Skin
-
-const RUNNER_DOWN = preload("uid://c0i1ik45p7rhh")
-const RUNNER_DOWN_RIGHT = preload("uid://cst3aklarj68")
-const RUNNER_RIGHT = preload("uid://b4etxv4c5w1mq")
-const RUNNER_UP = preload("uid://dtrvq16cx035")
-const RUNNER_UP_RIGHT = preload("uid://c7x3s5c2r5l86")
-
-const UP_LEFT = Vector2.UP + Vector2.LEFT
-const UP_RIGHT = Vector2.UP + Vector2.RIGHT
-const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
-const DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
-
 @export var max_speed := 600.0
 @export var acceleration := 1200.0
 @export var deceleration := 1080.0
+@onready var _runner_visual_red: RunnerVisual = %RunnerVisualRed
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left","move_right","move_up","move_down")
@@ -24,22 +12,16 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
-	
 	move_and_slide()
-	var direction_discrete := direction.sign()
-	match direction_discrete:
-		Vector2.RIGHT, Vector2.LEFT:
-			_skin.texture = RUNNER_RIGHT
-		Vector2.UP:
-			_skin.texture = RUNNER_UP
-		Vector2.DOWN:
-			_skin.texture = RUNNER_DOWN
-		UP_RIGHT, UP_LEFT:
-			_skin.texture = RUNNER_UP_RIGHT
-		DOWN_RIGHT, DOWN_LEFT:
-			_skin.texture = RUNNER_DOWN_RIGHT
 	
-	if direction_discrete.length() > 0:
-		_skin.flip_h = direction.x < 0.0
-	
+	if direction.length() > 0.0:
+		_runner_visual_red.angle = rotate_toward(_runner_visual_red.angle, direction.orthogonal().angle(),8.0 * delta)
+		var current_speed_percent := velocity.length() / max_speed
+		_runner_visual_red.animation_name = (
+			RunnerVisual.Animations.WALK
+			if current_speed_percent < 0.8
+			else RunnerVisual.Animations.RUN	
+		)
+	else:
+		_runner_visual_red.animation_name = RunnerVisual.Animations.IDLE
 	
