9c7f6db08b17982aede8fb56d7fad8084dc20126
[peeryo] <peeryo@sfusd.edu>
2025-12-11 21:20:43 -0800

    M9L9 completed

--- lessons/game.gd
+++ lessons/game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 @onready var _finish_line: FinishLine = %FinishLine
+@onready var _runner: Runner = %Runner
+@onready var _count_down: CountDown = %CountDown
 
 func _ready() -> void:
 	_finish_line.body_entered.connect(func (body: Node) -> void:
@@ -22,3 +24,10 @@ func _ready() -> void:
 	_finish_line.confettis_finished.connect(
 		get_tree().reload_current_scene.call_deferred
 	)
+	_count_down.start_counting()
+	_runner.set_physics_process(false)
+	_count_down.counting_finished.connect(
+		func() -> void:
+			_runner.set_physics_process(true) 
+	)
+	
